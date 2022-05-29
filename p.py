fc0104e4b9a999ef920f8cffc19ed38508a5bc4f
Abhishek Mani <aabhishekmani@gmail.com>
2022-05-29 14:55:35 +0530

    new

---
+++ p.py
@@ -0,0 +1,7 @@
+from urllib.request import urlopen
+with urlopen('http://worldtimeapi.org/api/timezone/etc/UTC.txt') as response:
+    for line in response:
+        line = line.decode()             # Convert bytes to a str
+        if line.startswith('datetime'):
+            print(line.rstrip())         # Remove trailing newline
+
