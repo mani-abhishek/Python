566712022890c6466337e897ead205b127dc6cc9
Abhishek Mani <aabhishekmani@gmail.com>
2022-06-06 23:56:30 +0530

    Fact

--- fact.py
+++ fact.py
@@ -3,9 +3,9 @@ def recur_factorial(n):
        return n  
    else:  
        return n*recur_factorial(n-1)  
-# take input from the user  
+ 
 num = int(input("Enter a number: "))  
-# check is the number is negative  
+
 if num < 0:  
    print("Sorry, factorial does not exist for negative numbers")  
 elif num == 0:  
